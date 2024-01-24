b0939b954320dc44969918306876cf89dfebe797
JeongJaeUk <skstk465@gmail.com>
2024-01-25 00:38:40 +0900

    solve until 3.11

--- src/main/kotlin/Chapter3/Chapter3.kt
+++ src/main/kotlin/Chapter3/Chapter3.kt
@@ -1,5 +1,10 @@
 package Chapter3
 
+
+import Chapter3.List.Companion.drop
+import Chapter3.List.Companion.dropWhile
+import Chapter3.List.Companion.init
+import Chapter3.List.Companion.length
 import Chapter3.List.Companion.setHead
 import Chapter3.List.Companion.tail
 
@@ -7,10 +12,19 @@ fun main() {
     var testList = List.of(1,2,3,4)
     val tailList = tail(testList)
 
-    println(tailList)
-
     val setHeadList = setHead(testList, 3)
     println(setHeadList)
+
+    val dropedList = drop(testList, 3)
+    println(dropedList)
+
+    val dropedWhileList = dropWhile(testList, {n -> n < 3})
+    println(dropedWhileList)
+
+    val initedList = init(testList)
+    println(initedList)
+
+    println(length(testList))
 }
 
 sealed class List<out A> {
@@ -20,19 +34,34 @@ sealed class List<out A> {
             return if (aa.isEmpty()) Nil else Cons(aa[0], of(*tail))
         }
 
-        fun sum(ints: List<Int>): Int =
-            when (ints) {
-                is Nil -> 0
-                is Cons -> ints.head + sum(ints.tail)
+        fun sum(ints: List<Int>): Int =  when (ints) {
+            is Nil -> 0
+            is Cons -> ints.head + sum(ints.tail)
+        }
+
+        fun product(doubles: List<Double>): Double = when (doubles) {
+            is Nil -> 1.0
+            is Cons -> doubles.head * product(doubles.tail)
+        }
+
+        fun <A, B> foldRight(xs: List<A>, z: B, f: (A, B) -> B): B = when(xs) {
+            is Nil -> z
+            is Cons -> f(xs.head, foldRight(xs.tail, z, f))
+        }
+
+        fun sum2(ints: List<Int>): Int =
+            foldRight(ints, 0, { a, b -> a + b})
+
+        fun product2(doubles: List<Double>): Double =
+            foldRight(doubles, 1.0, { a, b -> a * b})
+
+        fun <A> append(a1: List<A>, a2: List<A>): List<A> =
+            when (a1) {
+                is Nil -> a2
+                is Cons -> Cons(a1.head, append(a1.tail, a2))
             }
 
-        fun product(doubles: List<Double>): Double =
-            when (doubles) {
-                is Nil -> 1.0
-                is Cons ->
-                    if (doubles.head == 0.0) 0.0
-                    else doubles.head * product(doubles.tail)
-            }
+        fun <A> empty(): List<A> = Nil
 
         // 3.1
         fun <A> tail(xs: List<A>): List<A> =
@@ -47,6 +76,56 @@ sealed class List<out A> {
                 is Nil -> Nil
                 is Cons -> Cons(x, xs.tail)
             }
+
+        // 3.3
+        fun <A> drop(l: List<A>, n: Int): List<A> =
+            if(n == 0) l
+            else when (l) {
+                is Cons -> drop(l.tail, n - 1)
+                is Nil -> Nil
+            }
+
+        // 3.4
+        fun <A> dropWhile(l: List<A>, f: (A) -> Boolean): List<A> =
+            when(l) {
+                is Cons -> if (f(l.head)) dropWhile(l.tail, f) else l
+                is Nil -> Nil
+            }
+
+        // 3.5
+        fun <A> init(l: List<A>): List<A> =
+            when(l) {
+                is Cons ->
+                    if (l.tail == Nil) Nil
+                    else Cons(l.head, init(l.tail))
+                is Nil -> Nil
+            }
+
+        // 3.8
+        fun <A> length(xs: List<A>): Int =
+            foldRight(xs, 0, { _, x -> x + 1})
+
+        // 3.9
+        tailrec fun <A, B> foldLeft(xs: List<A>, z: B, f: (B, A) -> B): B =
+            when(xs) {
+                is Nil -> z
+                is Cons -> foldLeft(xs.tail, f(z, xs.head), f)
+            }
+
+        // 3.10
+        fun sumL(xs: List<Int>): Int =
+            foldLeft(xs, 0, { x, y -> x + y })
+
+        fun productL(xs: List<Double>): Double =
+            foldLeft(xs, 1.0, { x, y -> x * y })
+
+        fun <A> lengthL(xs: List<A>): Int =
+            foldLeft(xs, 0, { x, _ -> x + 1 })
+
+        // 3.11
+        fun <A> reverse(xs: List<A>): List<A> =
+            foldLeft(xs, List.empty(), { f: List<A>, z: A -> Cons(z, f) })
+        
     }
 }
 
